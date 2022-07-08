067b04e1903461a24a408745faa02017102f9490
Victor <psdtocss3@gmail.com>
2022-07-08 17:55:05 -0500

    home11

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 // import { completedTask } from "../features/slices/taskSlice";
 import { Link } from "react-router-dom";
 
-// import { MdModeEdit } from "react-icons/md";
+import { MdModeEdit } from "react-icons/md";
 // import { motion, AnimatePresence } from "framer-motion";
 
 const ToDoList = () => {
