b59b8edfee860dc56430061ee9e0e4eea3ba0e26
Victor <psdtocss3@gmail.com>
2022-07-08 17:48:12 -0500

    home8

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,3 +1,10 @@
+import { useDispatch } from "react-redux";
+import { completedTask } from "../features/slices/taskSlice";
+import { Link } from "react-router-dom";
+
+import { MdModeEdit } from "react-icons/md";
+import { motion, AnimatePresence } from "framer-motion";
+
 const ToDoList = () => {
   return <div>LISTA PRINCIPAL</div>;
 };
