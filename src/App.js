08b4980e59146bb6fe0864e8d35a87b47421e059
Victor <psdtocss3@gmail.com>
2022-07-08 16:49:49 -0500

    blabla

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import List from "./pages/List";
-import TaskEdit from "./pages/TaskEdit";
 
 function App() {
   return (
@@ -11,8 +9,6 @@ function App() {
           <Router>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="lista" element={<List />} />
-              <Route path="edit/:id" element={<TaskEdit />} />
             </Routes>
           </Router>
         </div>
